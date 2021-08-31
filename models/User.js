aae5fc7cd4d4681297f5309931ddc06089d19b04
Preshy <lukeprecious@gmail.com>
2021-08-31 21:58:59 +0100

    hide my env

--- models/User.js
+++ models/User.js
@@ -14,7 +14,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-
+  location: {   
+     type: String,    
+     default: "Port Harcourt", 
+      },
   date: {
     type: Date,
     default: Date.now,
